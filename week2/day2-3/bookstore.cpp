d8b140521280e77d65891e7251b574559d6b74e1
Viktor Södergren <vsodergr@volvocars.com>
2022-04-04 11:59:12 +0200

    book store progress

--- week2/day2-3/bookstore.cpp
+++ week2/day2-3/bookstore.cpp
@@ -33,35 +33,34 @@ Book::Book() {
   std::cout << "Enter book author: ";
   std::cin >> std::ws;
   std::getline(std::cin, in_string, '\n');
-  this->author;
+  this->author = in_string;
 
   std::cout << "Enter book title: ";
   std::cin >> std::ws;
   std::getline(std::cin, in_string, '\n');
-  this->title;
+  this->title = in_string;
 
   std::cout << "Enter book publisher: ";
   std::cin >> std::ws;
   std::getline(std::cin, in_string, '\n');
-  this->publisher;
+  this->publisher = in_string;
 
   int in_int{};
 
   std::cout << "Enter publishing year: ";
   std::cin >> in_int;
-  this->year;
+  this->year = in_int;
 
   std::cout << "Enter retail price: ";
   std::cin >> in_int;
-  this->price;
+  this->price = in_int;
 
   std::cout << "Enter initial stock: ";
   std::cin >> in_int;
-  this->current_stock;
+  this->current_stock = in_int;
 }
 
 Book::~Book() {}
-
 std::string Book::GetAuthor() { return this->author; }
 std::string Book::GetTitle() { return this->title; }
 std::string Book::GetPublisher() { return this->publisher; }
@@ -76,69 +75,96 @@ void Book::SetPrice(int &in) { this->price = in; }
 void Book::SetCurrentStock(int &in) { this->current_stock = in; }
 
 void PrintMenu() {
-  std::cout << " ~~ Menu ~~\n"
-            << "1) Enter new book\n"
-            << "2) List books\n"
-            << "3) Search books\n"
-            << "4) By a book\n"
-            << "5) Edit existing book\n"
-            << "5) Delete existing book\n"
-            << "7) Exit\n"
-            << " ~~ Choose: ";
+  std::cout << "\n"
+            << " ~~ Menu ~~\n"
+            << "    1) Enter new book\n"
+            << "    2) List books\n"
+            << "    3) Search books\n"
+            << "    4) By a book\n"
+            << "    5) Edit existing book\n"
+            << "    6) Delete existing book\n"
+            << "    7) Exit\n"
+            << " ~~ User input: ";
+}
+
+void PrintBook(Book &book) {
+  std::cout << "\tAuthor:      " << book.GetAuthor() << "\n"
+            << "\tTitle:       " << book.GetTitle() << "\n"
+            << "\tPublisher:   " << book.GetPublisher() << "\n"
+            << "\tPubl. year:  " << book.GetYear() << "\n"
+            << "\tPrice:       " << book.GetPrice() << "\n"
+            << "\tCurr. stock: " << book.GetCurrentStock() << "\n"
+            << "\n";
 }
 
 int main() {
+  // Main regirtry of inventory.
   std::vector<Book> inventory{};
 
-  int menu_input{};
-  PrintMenu();
-  std::cin >> menu_input;
+  // Running condition. Set false at exit option.
+  bool running{true};
 
-  switch (menu_input) {
-    case kNew: {
-      Book b{};
-      inventory.push_back(b);
-      break;
-    }
-    case kList: {
-      if (inventory.size() > 0) {
-        for (auto book : inventory) {
-          std::cout << "Author:\t" << book.GetAuthor() << "\n"
-                    << "Title:\t" << book.GetTitle() << "\n"
-                    << "Publicher:\t" << book.GetPublisher() << "\n"
-                    << "Publ. year:\t" << book.GetYear() << "\n"
-                    << "Price:\t" << book.GetPrice() << "\n"
-                    << "Curr. stock:\t" << book.GetCurrentStock() << "\n";
+  while (running) {
+    int menu_input{};
+    PrintMenu();
+    std::cin >> menu_input;
+
+    switch (menu_input) {
+      case kNew: {
+        Book b{};
+        inventory.push_back(b);
+        break;
+      }
+      case kList: {
+        std::cout << "\n"
+                  << "Listing books in registry: \n";
+
+        if (inventory.size() > 0) {
+          for (auto &book : inventory) PrintBook(book);
+        } else {
+          std::cout << "Inventory empty. Nothing to list.\n";
         }
-      } else {
-        std::cout << "Inventory empty. Nothing to list.\n";
+        break;
       }
-      break;
-    }
-    case kSearch: {
-      std::cout << "Search\n";
-      break;
-    }
-    case kBuy: {
-      std::cout << "Buy\n";
-      break;
-    }
-    case kEdit: {
-      std::cout << "Edit\n";
-      break;
-    }
-    case kDelete: {
-      if (inventory.size() > 0) {
-        std::cout << "Deleting last entry.\n";
-        inventory.pop_back();
-      } else {
-        std::cout << "Inventory empty. Nothing to delete.\n";
+      case kSearch: {
+        std::cout << "Search\n";
+
+        break;
       }
-      break;
-    }
-    case kExit: {
-      std::cout << "Exit\n";
-      break;
+      case kBuy: {
+        std::cout << "Buy\n";
+        break;
+      }
+      case kEdit: {
+        std::cout << "Edit\n";
+        break;
+      }
+      case kDelete: {
+        std::cout << "\n";
+
+        if (inventory.size() > 0) {
+          std::cout << "Deleting last entry.\n";
+          inventory.pop_back();
+        } else {
+          std::cout << "Inventory empty. Nothing to delete.\n";
+        }
+        break;
+      }
+      case kExit: {
+        int confirmation{};
+
+        std::cout << "\n"
+                  << " ~~ Are you sure you want to exit? \n"
+                  << "    1) No! Back to the menu.\n"
+                  << "    2) Yes! Exit the book store.\n"
+                  << " ~~ User confirmation: ";
+        std::cin >> confirmation;
+
+        if (2 == confirmation) running = false;
+        break;
+      }
     }
   }
+
+  std::cout << "Thank you for your visit! Good bye.\n";
 }
