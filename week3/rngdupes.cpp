e99f9cbf0611c8441d86487f016a395b953a4c7c
Viktor Södergren <viktor.sodergren@volvocars.com>
2022-04-07 15:39:43 +0200

    add assignment sorted duplicate count

--- week3/rngdupes.cpp
+++ week3/rngdupes.cpp
@@ -1,26 +1,47 @@
+/*
+ * Generate random numbers between 0-33, ten thousand times, then print how many
+ * duplicates of each number you have generated -- print in ascending order.
+ */
+
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <random>
-#include <set>
-#include <map>
+#include <vector>
 
 const int n = 10000;
 const int n_bins = 34;
 
+bool compare(std::pair<int, int>& a, std::pair<int, int>& b) {
+  return a.second < b.second;
+}
+
 int main() {
   srand(time(0));
-  std::set<int> rand_nbrs{n, 0};
-  std::map<int, int> histogram({n_bins, 0});
-  std::map<int, int>::iterator itr;
+  std::vector<std::pair<int, int>> histogram(n_bins, {0, 0});
 
-  for (int i = 0; i < n; i++) rand_nbrs.emplace(rand() % 34);
-
-  for (itr = histogram.begin(); itr != histogram.end(); itr++) {
-    std::cout << itr->first << " " << itr->second << "\n";
+  for (size_t i = 0; i < n; i++) {
+    int rnr = rand() % 34;
+    histogram.at(rnr).first = rnr;
+    histogram.at(rnr).second++;
   }
 
-  // for (int i = 0; i < histogram.size(); i++) {
-  //   std::cout << i << " genereated " << histogram.count(i) << "\n";
-  // }
+  std::cout << "\n";
+
+  // std::sort(histogram.begin(), histogram.end(), compare);
+  // I learned that a lambda could be aproppriate for comparing.
+  std::sort(histogram.begin(), histogram.end(),
+            [](const auto& a, const auto& b) { return a.second < b.second; });
+
+  std::cout << "Sorted set of " << n << " randomized numbers (0-33):\n";
+  std::cout << std::setw(3) << "Key"
+            << ":" << std::setw(6) << "Value"
+            << "\n";
+
+  for (const auto& each : histogram) {
+    std::cout << std::setw(3) << each.first << ":" << std::setw(6)
+              << each.second << "\n";
+  }
 
   std::cout << "\n";
 }
